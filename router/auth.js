069c8bcf1b150ab5d68a8592555d0fae0c770381
LEGENDO0991 <97464074+LEGENDO0991@users.noreply.github.com>
2024-12-24 22:09:11 +0530

    Update auth.js

--- router/auth.js
+++ router/auth.js
@@ -1,9 +1,18 @@
 import { register,login, verifyOtp, checkAuth } from './../controllers/auth.js'
 import { isAuthenticated } from './../middlewares/auth.js';
+import { rateLimit } from "express-rate-limit";
+
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    limit: 5,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message:"Too many attempts please try again after 5 minutes"
+})
 
 export default (router) => {
-    router.post('/api/auth/login', login)
-    router.post('/api/auth/register', register)
-    router.post('/api/auth/verify-otp', verifyOtp)
+    router.post('/api/auth/login', limiter,login)
+    router.post('/api/auth/register',limiter, register)
+    router.post('/api/auth/verify-otp',limiter, verifyOtp)
     router.post('/api/auth/check', isAuthenticated, checkAuth)
 }
